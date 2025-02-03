64dca4e1cd8060247cf32e6b11c53e650081f12c
Atugov <bugmisha@gmail.com>
2025-02-03 14:35:00 +0000

    test8

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ TOKEN: Final = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 SUPABASE_URL: Final = os.getenv("SUPABASE_URL")
 SUPABASE_KEY: Final = os.getenv("SUPABASE_KEY")
-# Test6
+# Test8
