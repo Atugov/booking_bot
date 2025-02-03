f67e7c8564fc4fcff434600b20d2b938cd1d2726
MichaelBugrov <73523121+Atugov@users.noreply.github.com>
2025-02-03 15:01:29 +0000

    Feat/permissions (#1)

--- config.py
+++ config.py
@@ -9,4 +9,3 @@ TOKEN: Final = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 SUPABASE_URL: Final = os.getenv("SUPABASE_URL")
 SUPABASE_KEY: Final = os.getenv("SUPABASE_KEY")
-# Test
